5e7360a7629847a4a0b82158c553b44c8313e5bd
Viviani harima <viviharima@hotmail.com>
2024-02-25 17:47:17 -0300

    exercício 10

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ function alerta (){
 
 alerta ()
 
+let curiosidadeAtual = 0;
+const curiosidades = [
+    "Curiosidade: Jogo jogos de ritmo",
+    "Curiosidade: Estudo japonês",
+    "Curiosidade: Estou adorando aprender JavaScript",
+    "Curiosidade: Estudo gastronomia no IFSC",
+    "Curiosidade: Pratico Kyudo (arqueria tradicional japonesa)"
+]
 function alterarTexto(){
 let curiosidadeNova = document.getElementById("curiosidade");
 
-    curiosidadeNova.textContent = "Curiosidade: estudo japonês";
+    curiosidadeNova.textContent = curiosidades[curiosidadeAtual];
+    curiosidadeAtual = (curiosidadeAtual + 1) % curiosidades.length;
 
 }
 
